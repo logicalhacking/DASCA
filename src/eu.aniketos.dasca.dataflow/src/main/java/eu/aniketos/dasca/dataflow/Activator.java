02fd55fb36da3897d6bc47f43ee5b5d3166c31bf
Achim D. Brucker <adbrucker@0x5f.org>
2016-08-21 15:51:27 +0100

    Updated plugin id.

--- src/eu.aniketos.dasca.dataflow/src/main/java/eu/aniketos/dasca/dataflow/Activator.java
+++ src/eu.aniketos.dasca.dataflow/src/main/java/eu/aniketos/dasca/dataflow/Activator.java
@@ -19,7 +19,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
     // The plug-in ID
-    public static final String PLUGIN_ID = "com.sap.research.analysis.wala.sourcecode"; //$NON-NLS-1$
+    public static final String PLUGIN_ID = "eu.aniketos.dasca.dataflow"; //$NON-NLS-1$
 
     // The shared instance
     private static Activator plugin;
