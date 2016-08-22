530c6fd38830ddaa5e0e66e8302f6d74a9309448
Achim D. Brucker <adbrucker@0x5f.org>
2016-08-22 10:02:03 +0100

    Updated copyright information.

--- src/eu.aniketos.dasca.dataflow/src/main/java/eu/aniketos/dasca/dataflow/actions/AnalyzeSourceCode.java
+++ src/eu.aniketos.dasca.dataflow/src/main/java/eu/aniketos/dasca/dataflow/actions/AnalyzeSourceCode.java
@@ -1,5 +1,6 @@
 /*
  * (C) Copyright 2010-2015 SAP SE.
+ *               2016      The University of Sheffield.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
