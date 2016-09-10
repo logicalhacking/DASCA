f2a31d9b1d275889855d326f9841c5ab1fbe7f3f
Achim D. Brucker <adbrucker@0x5f.org>
2016-09-10 19:32:00 +0100

    Normalized indentation/formatting.

--- src/eu.aniketos.dasca.crosslanguage/src/main/scala/eu/aniketos/dasca/crosslanguage/Main.scala
+++ src/eu.aniketos.dasca.crosslanguage/src/main/scala/eu/aniketos/dasca/crosslanguage/Main.scala
@@ -19,25 +19,25 @@ import org.slf4j.LoggerFactory
 import com.typesafe.scalalogging.Logger
 
 object Main {
-    def main(args: Array[String]): Unit = {
-        if (args.length < 1 || (args.length < 2 && args(0).charAt(0) == '-')) {
-            println("You must at least provide the path to the apk!")
-            return
-        }
-
-        val apk = if (args(0).charAt(0) == '-') new File(args(1)) else new File(args(0))
-
-                if (!apk.exists() || !apk.getCanonicalPath.endsWith(".apk")) {
-                    println("Please provide a valid apk file!")
-                    return
-                }
-
-                val options = if (args(0).charAt(0) == '-') Util.argsToOptions(args(0)) else List()
-                        implicit val logger = Logger(LoggerFactory.getLogger(getClass.toString))
-                        Util.time("Creation of the Cordova unified call graph", {
-                        val builder = CordovaCGBuilder(apk)
-                        builder.setOptions(options: _*)
-                        builder.createCallGraph
-                    })
+  def main(args: Array[String]): Unit = {
+    if (args.length < 1 || (args.length < 2 && args(0).charAt(0) == '-')) {
+      println("You must at least provide the path to the apk!")
+      return
     }
+
+    val apk = if (args(0).charAt(0) == '-') new File(args(1)) else new File(args(0))
+
+    if (!apk.exists() || !apk.getCanonicalPath.endsWith(".apk")) {
+      println("Please provide a valid apk file!")
+      return
+    }
+
+    val options = if (args(0).charAt(0) == '-') Util.argsToOptions(args(0)) else List()
+    implicit val logger = Logger(LoggerFactory.getLogger(getClass.toString))
+    Util.time("Creation of the Cordova unified call graph", {
+      val builder = CordovaCGBuilder(apk)
+      builder.setOptions(options: _*)
+      builder.createCallGraph
+    })
+  }
 }
