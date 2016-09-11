a590ded14bf162f389f51ad1514446def84595e5
Achim D. Brucker <adbrucker@0x5f.org>
2016-09-11 13:08:36 +0100

    Added simple example showing how to construct CallTrees.

--- src/eu.aniketos.dasca.crosslanguage/src/main/scala/eu/aniketos/dasca/crosslanguage/Main.scala
+++ src/eu.aniketos.dasca.crosslanguage/src/main/scala/eu/aniketos/dasca/crosslanguage/Main.scala
@@ -12,11 +12,15 @@ package eu.aniketos.dasca.crosslanguage
 
 import java.io.File
 import eu.aniketos.dasca.crosslanguage.builder._
+import eu.aniketos.dasca.crosslanguage.cg._
 import scala.collection.mutable.ListBuffer
 import java.lang.management.ManagementFactory
 import eu.aniketos.dasca.crosslanguage.util.Util
 import org.slf4j.LoggerFactory
 import com.typesafe.scalalogging.Logger
+import com.ibm.wala.ipa.callgraph.CGNode
+import com.ibm.wala.cast.js.loader.JavaScriptLoader
+import com.ibm.wala.util.strings.Atom
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -34,10 +38,41 @@ object Main {
 
     val options = if (args(0).charAt(0) == '-') Util.argsToOptions(args(0)) else List()
     implicit val logger = Logger(LoggerFactory.getLogger(getClass.toString))
+    var cg = null:MergedCallGraph
     Util.time("Creation of the Cordova unified call graph", {
       val builder = CordovaCGBuilder(apk)
       builder.setOptions(options: _*)
-      builder.createCallGraph
+      cg = builder.createCallGraph
+    })
+    
+    // compute sinks (JS entry points from index.js) and 
+    //         sources (execSQL statements)
+    val it = cg.iterator()
+    var sinks = List[CGNode]()
+    var roots = List[CGNode]()
+    val execSQL = Atom.findOrCreateUnicodeAtom("execSQL");
+    for(n <- it){
+      if (Util.isJavaNode(n)){
+        if (n.getMethod().getName.equals(execSQL)) { sinks = sinks.+:(n) } 
+      }else{
+        try{
+            if((n.getMethod().asInstanceOf[JavaScriptLoader#JavaScriptMethodObject]).getDeclaringClass()
+                      .getSourceFileName().endsWith("index.js"))
+         {roots = roots.+:(n) }
+        }catch{
+        case e:ClassCastException => {}
+        }
+      }
+    }
+    println ("Number of roots: "+roots.size)
+    roots.map { x => println("  "+x) }
+    println ("Number of sinks: "+sinks.size)
+    sinks.map { x => println("  "+x) }
+
+    Util.time("Creation of the CallForest", {
+      val callForest = CallTreeBuilder.buildCallForest(cg, 15, roots, sinks)
+      print ("Number of trees: "+callForest.size)
+      callForest.map { x => print ("\n"+x+"\n\n") }
     })
   }
 }
